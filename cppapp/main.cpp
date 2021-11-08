0f7704df4093e3d470a6d751a4af9e8c5acf746f
vaugha <73072657+vaugha@users.noreply.github.com>
2021-11-08 14:09:39 +0530

    first commit

---
+++ cppapp/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<conio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	cout<<"hello World!\n";
+}
---
+++ simplecpp/SimpleCppMain.cpp
@@ -0,0 +1,16 @@
+#include<simplecpp>
+
+main_program
+{
+//Write your code here
+initCanvas("welcome page",800,800);
+Turtle t;
+t.forward(100);
+wait(1);
+t.left(90);
+wait(1);
+t.forward(100);
+wait(1);
+t.right(90);
+wait(6);
+}
